7aa4766552e0fbeb86179ad7e32ce6f661783a90
denys.molyboha <denys.molyboha@cosmonova.net>
2024-02-10 23:22:15 +0200

    first commit, start

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
---
+++ models/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+import sqlalchemy
+from sqlalchemy import MetaData, Table, Column, Integer, String, JSON, TIMESTAMP, ForeignKey
+
+metadata = MetaData()
+
+roles = Table(
+    "roles",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permission", JSON)
+)
+
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("login", String, nullable=False),
+    Column("password", String, nullable=False),
+    Column("created", TIMESTAMP, default=datetime.utcnow),
+    Column("role_id", Integer, ForeignKey("roles.id")),
+)
+
+engine = sqlalchemy.create_engine()
+metadata.create_all()
